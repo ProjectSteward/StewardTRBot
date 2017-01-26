fb5a97a6447d2b894fdcd7bc6a4a7d8b97b5b305
nipon <nipon.chinathimat@thomsonreuters.com>
2017-01-26 13:31:59 +0700

    Updated example to read transaction log.

--- readlogs/Program.cs
+++ readlogs/Program.cs
@@ -1,19 +1,10 @@
-﻿using Autofac;
-using Microsoft.Bot.Builder.Azure;
-using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Builder.Dialogs.Internals;
-using Microsoft.Bot.Builder.History;
-using Microsoft.Bot.Connector;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Auth;
+﻿using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using static Microsoft.Bot.Builder.Azure.TableLogger;
 using Microsoft.Azure;
+using Microsoft.Bot.Connector;
+using Microsoft.WindowsAzure.Storage.Table.Protocol;
 
 namespace readlogs
 {
@@ -21,27 +12,73 @@ namespace readlogs
     {
         static void Main(string[] args)
         {
-           
-                         
-            CloudStorageAccount acc = CloudStorageAccount.Parse(
-     CloudConfigurationManager.GetSetting("logtableconnectionstring"));
+            var acc = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("logtableconnectionstring"));
             var tableClient = acc.CreateCloudTableClient();
-            var table = tableClient.GetTableReference("BotData");
+            var table = tableClient.GetTableReference("ActivitiesLogger");
             TableContinuationToken token = null;
 
-
-            var entities = new List<ActivityEntity>();
+            var qq = BuildQuery(null, null, new DateTime(2016, 01, 01)); // get conversation since date
             do
             {
-                var queryResult = table.ExecuteQuerySegmented(new TableQuery<ActivityEntity>(), token);
-                entities.AddRange(queryResult.Results);
-                token = queryResult.ContinuationToken;
-                foreach (var activity in entities)
+                var queryResult = table.ExecuteQuerySegmented(qq, (pKey, rowKey, timestamp, properties, etag) =>
                 {
-                   // activity.ReadEntity(activity.WriteEntity(null), null);
-                }               
+                    var entity = new ActivityEntity();
+                    entity.ReadEntity(properties, null);
+                    return entity.Activity;
+                }, token);
+ 
+                foreach (var result in queryResult)
+                {
+                    var activity =  result as Activity;
+                    if (activity == null)
+                        continue;
+
+                    Console.WriteLine("{0} --> {1} @{2}:\t {3}\r\n\r\n", 
+                        GetSenderText(activity.From), 
+                        GetSenderText(activity.Recipient), 
+                        activity.Timestamp, activity.Text);
+                }
+                token = queryResult.ContinuationToken;
             } while (token != null);
 
+            Console.ReadLine();
         }
+
+        private static string GetSenderText(ChannelAccount ca)
+        {
+            return string.IsNullOrEmpty(ca.Name) ? ca.Id : ca.Name;
+        }
+
+        // copied from TableLogger.cs in Microsoft.Bot.Builder.Azure
+        private static TableQuery BuildQuery(string channelId, string conversationId, DateTime oldest)
+        {
+            var query = new TableQuery();
+            string filter = null;
+            if (channelId != null && conversationId != null)
+            {
+                var pkey = ActivityEntity.GeneratePartitionKey(channelId, conversationId);
+                filter = TableQuery.GenerateFilterCondition(TableConstants.PartitionKey, QueryComparisons.Equal, pkey);
+            }
+            else if (channelId != null)
+            {
+                var pkey = ActivityEntity.GeneratePartitionKey(channelId, "");
+                filter = TableQuery.GenerateFilterCondition(TableConstants.PartitionKey, QueryComparisons.GreaterThanOrEqual, pkey);
+            }
+            if (oldest != default(DateTime))
+            {
+                var rowKey = ActivityEntity.GenerateRowKey(oldest);
+                var rowFilter = TableQuery.GenerateFilterCondition(TableConstants.RowKey, QueryComparisons.GreaterThanOrEqual, rowKey);
+                filter = filter == null ? rowFilter : TableQuery.CombineFilters(filter, TableOperators.And, rowFilter);
+            }
+            if (filter != null)
+            {
+                query.Where(filter);
+            }
+            return query;
+        }
+        
     }
+
+    
 }
